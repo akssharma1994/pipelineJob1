8b3a4ce699dc0186375d3e531bfae83737825fc6
akssharma1994 <akssharma1994@gmail.com>
2021-12-13 16:12:08 +0530

    adding changes in jenkinsfile

--- script.groovy
+++ script.groovy
@@ -2,9 +2,9 @@ def buildApp(){
     echo "This is building the version ${params.VERSION}"
 }
 def testApp(){
-    echo "Testing the application version ${params.exectueTests}"
+    echo "Testing the application version ${params.VERSION}"
 }
 def deployAPP(){
-    echo "Deploying the version ${param.VERSION}" 
+    echo "Deploying the version ${params.VERSION}" 
 }
 return this
